09cad9c3780fbf0fcb36f45069e527fa62ca5dc3
Vladimir Galindo <vlad.galindo@gmail.com>
2020-03-02 10:44:23 -0500

    real initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const moment = require('moment');
+const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
+
+const showMessage = (req, res) => {
+    const message = `your message is: ${req.body.msg}`;
+    res.send(message)
+};
+
+const getTime = (req, res) => {
+    res.send(moment().format('LLLL'));
+};
+
+const envVars = (req, res) => {
+    res.send(process.env)
+};
+
+const getSecrets = async (req, res) => {
+    const PROJECT = 'cloud-function-test-269618';
+    const client = new SecretManagerServiceClient();
+
+    // Access the secret.
+    const [accessResponse] = await client.accessSecretVersion({
+        name: 'my-secret',
+    });
+
+    const responsePayload = accessResponse.payload.data.toString('utf8');
+    const message = `Payload: ${responsePayload}`;
+    console.info(message);
+    res.send(message);
+};
+
+module.exports = {
+    showMessage,
+    getTime,
+    envVars,
+    getSecrets,
+};
