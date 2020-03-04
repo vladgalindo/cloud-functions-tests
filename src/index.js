f174eebe719d864890d355d9ef8e310c52ebbee7
Vladimir Galindo <vlad.galindo@gmail.com>
2020-03-04 10:54:39 -0500

    trying to get secrets

--- src/index.js
+++ src/index.js
@@ -18,10 +18,22 @@ const envVars = (req, res) => {
 };
 
 const getSecrets = async (req, res) => {
-    const PROJECT = 'cloud-function-test-269618';
+    const PROJECT = 'projects/cloud-function-test-269618';
     const client = new SecretManagerServiceClient();
 
-    // Access the secret.
+    const [secrets] = await client.listSecrets({
+        parent: PROJECT,
+    });
+
+    secrets.forEach(secret => {
+        const policy = secret.replication.replication;
+        console.log(`${secret.name} (${policy})`);
+    });
+
+    res.send(secrets);
+
+
+    /*// Access the secret.
     const [accessResponse] = await client.accessSecretVersion({
         name: 'my-secret',
     });
@@ -29,7 +41,7 @@ const getSecrets = async (req, res) => {
     const responsePayload = accessResponse.payload.data.toString('utf8');
     const message = `Payload: ${responsePayload}`;
     console.info(message);
-    res.send(message);
+    res.send(message);*/
 };
 
 module.exports = {
